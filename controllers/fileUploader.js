f2bc19ebaafd2a4bff5417e25a8a119c119838f9
zakizaidi55 <zakizaidi55@gmail.com>
2024-01-18 03:27:17 +0530

    write down the controller for videoUpload and image size reduces

--- controllers/fileUploader.js
+++ controllers/fileUploader.js
@@ -28,8 +28,13 @@ function isFileTypeSupported (type, supportedType) {
     return supportedType.includes(type);
 }
 
-async function uploadFileToCloudinary(file, folder) {
+async function uploadFileToCloudinary(file, folder, quality) {
     const options = {folder};
+
+    if(quality) {
+        options.quality = quality;
+    }
+    options.resource_type = "auto";
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
 
@@ -83,3 +88,98 @@ exports.imagaUpload = async(req, res) => {
         })
     }
 };
+
+exports.videoUpload = async(req, res) => {
+    try {
+        const {name, tags, email} = req.body;
+        console.log(name, tags, email);
+
+        const file = req.File.videoFile;
+
+        // validation
+        const supportedType = ["mp4", "mov"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+        console.log("file type", fileType);
+
+        if(!isFileTypeSupported(fileType, supportedType)) {
+            return re.status(400).json({
+                success:false,
+                message:"Format type not supported",
+            })
+        }
+
+
+        console.log("Uploading to the cloud ");
+        const response = await uploadFileToCloudinary(file, "zakiData");
+        console.log("Response", response);
+
+        const fileData = await File.create({
+            name,
+            tags, 
+            email,
+            videoUrl: response.secure_url,
+        })
+        
+        res.status(201).json({
+            success:true,
+            imageUrl:response.secure_url,
+            message:"Video uploaded successfully",
+        })
+
+    } catch(err) {
+        console.error(err)
+        res.status(400).json({
+            success:false,
+            message:"Something went wrong",
+        })
+    }
+};
+
+exports.imageSizeReducer =async (req, res) => {
+    try {
+        const {name, tags, email} = req.body;
+        console.log(name, tags, email);
+
+        const file = req.files.imageFile;
+        console.log(file);
+
+        // validation
+        const supportedType = ["jpg", "jpeg", "png"];
+
+        const fileType = file.name.split('.')[1].toLowerCase();
+        console.log("File format ", fileType);
+        
+        if(!isFileTypeSupported(fileType, supportedType)) {
+            return res.status(400).json({
+                success:false,
+                message:'File format is not suppported',
+            })
+        }
+
+        console.log("Uploading to the cloud ");
+        const response = await uploadFileToCloudinary(file, "zakiData", 30);
+        console.log("Response", response);
+
+        // db m entry
+        const fileData = await File.create({
+            name,
+            tags, 
+            email,
+            imageUrl: response.secure_url,
+        })
+        
+        res.status(201).json({
+            success:true,
+            imageUrl:response.secure_url,
+            message:"Image uploaded successfully",
+        })
+
+
+    } catch(error) {
+        console.error(error);
+        res.status(400).json({
+            success:false,
+            message:'Something went wrong ',
+        })
+    }
+}
