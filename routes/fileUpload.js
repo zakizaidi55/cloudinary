3a41fc8392e8c85a5386c253cea5ac4da133528e
zakizaidi55 <zakizaidi55@gmail.com>
2024-01-17 17:02:58 +0530

    add route for the image upload to cloudinary

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {localFileUpload} = require("../controllers/fileUploader");
+const {localFileUpload, imagaUpload} = require("../controllers/fileUploader");
 
 router.post("/localFileUpload", localFileUpload);
+router.post("/imagaUpload", imagaUpload);
 
 module.exports = router;
