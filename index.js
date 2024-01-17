bfcf00ac0a295512ac4dd2176259b7feb18037cc
zakizaidi55 <zakizaidi55@gmail.com>
2024-01-17 17:03:17 +0530

    flag added in the clodinary instance

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 3000;
 // middleware
 app.use(express.json());
 const fileupload = require("express-fileupload");
-app.use(fileupload());
+app.use(fileupload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/'
+}));
 
 // db connect
 const db = require("./config/database");
