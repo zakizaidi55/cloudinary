74140d1c97d750c22707c838049a68998a3c9750
zakizaidi55 <zakizaidi55@gmail.com>
2024-01-18 03:26:50 +0530

    add videoUpload and imagesizze reducer route

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {localFileUpload, imagaUpload} = require("../controllers/fileUploader");
+const {localFileUpload, imagaUpload, videoUpload, imageSizeReducer} = require("../controllers/fileUploader");
 
 router.post("/localFileUpload", localFileUpload);
 router.post("/imagaUpload", imagaUpload);
+router.post("/videoUpload", videoUpload);
+router.post("/imageSizeReducer", imageSizeReducer);
 
 module.exports = router;
